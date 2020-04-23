1d45ada27caaa9d0bd9d4aa5063fc0ecd3b6d1c5
Mia Kennedy <mikkenne@ucsc.edu>
2020-04-22 17:15:31 -0700

    added name

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//Mia Kennedy
 //Implemented both S-Rank Tier Modifications:
 //Implement a simultaneous two-player mode (50)
 //Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50)
